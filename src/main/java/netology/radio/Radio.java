41f0ae6b60da10bde93d2bf0f48278ac30077945
Tohage89 <avgorin89@gmail.com>
2025-03-30 15:13:06 +0300

    changeClassRadio

--- src/main/java/netology/radio/Radio.java
+++ src/main/java/netology/radio/Radio.java
@@ -1,12 +1,10 @@
 package netology.radio;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
+
 
 @Data
-@AllArgsConstructor
 public class Radio {
     private final int numberOfStations;
     @Getter
